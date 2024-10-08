972b509ce769a8245996f2044cbbfb39f484c9d6
Eloi Dewynter--Rebitz <griszou12@gmail.com>
2024-10-08 21:19:58 +0200

    nearly done

--- App/signing_manager.py
+++ App/signing_manager.py
@@ -5,6 +5,18 @@ import re
 
 """Loader"""
 def load(user_db):
+    print(request.form)
+
+    feedback_messages = manage_signing(user_db)
+
+    if feedback_messages["password_field"] == "Password valid":
+        return redirect("/home")
+
+    return render_template("signing.html", feedback_messages=feedback_messages)
+
+
+"""Sign management"""
+def manage_signing(user_db):
     # Allows the html to render the submit button for the email, if the email's correct it will be changed later anyway
     session["signing_phase"] = 0
 
@@ -15,92 +27,95 @@ def load(user_db):
         "password_field" : None
     }
 
-
     # Email :
     # Get input and remove whitespaces, lowercase it
-    email = get_input("email")
-    if email:
-        email = email.replace(" ", "").lower()
-
+    email = get_input('email')
     # Check email, if it's all good determine state and phase.
     is_email_valid = check_email(email)
     if is_email_valid is True:
         feedback_messages["email_field"] = "Email valid"
         session['signing_state'] = determine_signing_state(user_db, email)
         session['signing_phase'] = determine_signing_phase(session.get("signing_state"))
+        print(f"signing_phase : {session['signing_phase']}")
     elif is_email_valid is False:
         session["signing_state"] = None
         feedback_messages["email_field"] = "Please enter a valid email address"
     else: # If it's None (no query (first launch or clear cookies))
         pass
 
+
+    # Username :
+    username = get_input('username')
+    is_username_valid = check_username(username)
+
+    if is_username_valid is True:
+        feedback_messages["username_field"] = "Username valid"
+    elif is_username_valid == "Username must be at least 3 characters" or is_username_valid == "Username must be at most 20 characters":
+        feedback_messages["username_field"] = is_username_valid
+    else:
+        pass # -> No query
+
+    signing_state = session.get('signing_state')
     # Sign_up :
     # If a state was determined watch for username, password. If both input are filled and ok -> create user, connect
-    if session.get("signing_state") == "sign_up":
-        if session.get("signing_phase"):
-            if session.get("signing_phase") > 0:
-                username = get_input("username")
-                is_username_valid = check_username(username)
-                if is_username_valid is True:
-                    feedback_messages["username_field"] = "username valid"
-                else:
-                    if is_username_valid == "username must be at least 3 characters" or is_username_valid == "username must be at most 20 characters":
-                        feedback_messages["username_field"] = is_username_valid
-            if session.get("signing_phase") > 1:
-                password = get_input("password")
-                is_password_valid = check_password(password)
-                if is_password_valid is not None:
-                    if is_password_valid == "password must be at least 8 characters":
-                        feedback_messages["password_field"] = is_password_valid
-                    if is_password_valid == True:
-                        feedback_messages["password_field"] = "Password valid"
-                        password = hash_password(password)
-                        user_db.create_user(email, username, password)
-                        session.clear()
-                        session["email"], session["username"], session["login_state"] = email, username, True
-                        return redirect("/home")
-
+    if signing_state == "sign_up":
+        feedback_messages = sign_up(user_db, feedback_messages)
     # Sign_in :
     # If a state was determined, look for password, compare it to the hash, if it's all right -> connect
-    elif session.get("signing_state") == "sign_in":
-        if session.get("signing_phase") > 0:
-            password = get_input("password")
-            if password:
-                password = password.encode("utf-8")
-                if bcrypt.checkpw(password, user_db.get_password_hash_by_email(email)):
-                    feedback_messages["password_field"] = "Password valid"
-                    session.clear()
-                    session["email"], session["username"], session["login_state"] = email, user_db.get_username_by_email(email), True
-                    return redirect("/home")
-                else:
-                    feedback_messages["password_field"] = "Password invalid"
+    elif signing_state == "sign_in":
+        feedback_messages = sign_in(user_db, feedback_messages)
+
+    return feedback_messages
 
 
-    return render_template("signing.html", feedback_messages=feedback_messages)
+def sign_up(user_db, feedback_messages):
+    signing_phase = session.get('signing_phase')
+    email = session.get("email") ; username = session.get("username")
+    # Signing_phase is set to at least 1 if state is defined
+    if signing_phase > 1:
+        password = request.form.get("password")
+        is_password_valid = check_password(password)
+        if is_password_valid == "Password must be at least 8 characters":
+            feedback_messages["password_field"] = is_password_valid
+        if is_password_valid is True:
+            feedback_messages["password_field"] = "Password valid"
+            password = hash_password(password)
+            user_db.create_user(email, username, password)
+            session.clear()
+            session["email"], session["username"], session["login_state"] = email, username, True
+        # If password is None -> pass
+    return feedback_messages
 
+def sign_in(user_db, feedback_messages):
+    signing_phase = session.get('signing_phase')
+    email = session.get("email") ; username = session.get("username")
+    if signing_phase > 0:
+        password = get_input("password")
+        if password:
+            password = password.encode("utf-8")
+            if bcrypt.checkpw(password, user_db.get_password_hash_by_email(email)):
+                feedback_messages["password_field"] = "Password valid"
+                session.clear()
+                session["email"], session["username"], session["login_state"] = email, user_db.get_username_by_email(
+                    email), True
+            else:
+                feedback_messages["password_field"] = "Password invalid"
+    return feedback_messages
 
-"""Code"""
+"""UTILS"""
+"""Signing phase and state"""
 def determine_signing_phase(signing_state):
     if signing_state == "sign_in":
         return 1
     elif signing_state == "sign_up":
-        if get_input("username"):
+        username_check = check_username(request.form.get("username"))
+        if username_check == "username must be at least 3 characters" or username_check == "username must be at most 20 characters":
+            return 1
+        elif username_check == True:
             return 2
         else:
             return 1
 
-def get_input(field):
-    # See the readme to know why i read from form or cookies
-    if request.form.get(field) is None:
-        user_input = session.get(field)
-    else:
-        user_input = request.form.get(field)
-        session[field] = user_input
-
-    return user_input
-
-
-"""Email related"""
 # If email exists -> sign in, else -> sign up
 def determine_signing_state(user_db, user_email):
     all_emails = user_db.fetch_all_emails()
@@ -111,6 +126,24 @@ def determine_signing_state(user_db, user_email):
             break
 
     return is_email_existing
+
+
+# Inputs related (get and check)
+def get_input(field):
+    TO_FORMAT_FIELDS = ["username", "email"]
+    user_input = request.form.get(field)
+
+    # See the readme to know why I read from form or cookies
+    if user_input is None or user_input == "" or user_input == " ":
+        user_input = session.get(field)
+    else:
+        # Remove whitespaces, lowercase input if necessary
+        if field in TO_FORMAT_FIELDS:
+            user_input = user_input.replace(" ", "").lower()
+
+        session[field] = user_input
+
+    return user_input
 
 
 def check_email(email):
@@ -124,27 +157,27 @@ def check_email(email):
         return None
 
 def check_username(username):
-    if username is not None:
+    if username is not None and username != "" and username != " ":
         if len(username) < 3:
-            return "username must be at least 3 characters"
+            return "Username must be at least 3 characters"
         elif len(username) > 20:
-            return "username must be at most 20 characters"
+            return "Username must be at most 20 characters"
         else:
             return True
     else:
             return None
 
 def check_password(password):
-    # password_regex = "" # usable but i'm not gonna
-    if password is not None:
+    # password_regex = "" # usable but i'm not using it for now
+    if password is not None and password != "" and password != " ":
         if len(password) < 8:
-            return "password must be at least 8 characters"
+            return "Password must be at least 8 characters"
         else:
             return True
     else:
         return None
 
-
+"""Bcrypt password encryption"""
 def hash_password(password):
     # str -> bstr
     password_bytes = password.encode('utf-8')
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 """Imports"""
+import os
 from os import getenv
 
-from flask import Flask, redirect, session, render_template_string, request, g
+from flask import Flask, redirect, session, render_template_string, request, g, send_from_directory, render_template
+from flask.globals import request_ctx
+
 from App import index, home, signing_manager
 from App.utils.db.user_db import *
 
+global request_counter
+globals()['request_counter'] = 0
 
-"""Main flask_app"""
 if __name__ == "__main__":
     # Flask and session cookies secret key init
     flask_app = Flask("sign.in.up - sqlite - flask")
     flask_app.secret_key = b'\xe5\xef\x92\x89\xc9v\x18\x14P\xbf\x88\xd7\x19\xff\x8b\x1aH\xd2\x15\xc7\xd0\x1cv\xf6' # os.urandom(24)
-
 
     """DB MANAGEMENT"""
     # Open db when needed
@@ -31,28 +34,44 @@ if __name__ == "__main__":
             print(f"Closing user_db : {exception}")
             db.quit_db()
 
+
     @flask_app.before_request
     def check_if_user_is_connected():
-        EXCLUDED_ROUTES = ["route_sign_in_or_up", "route_index"]  # add routes as needed
-        route_to_load = request.endpoint # To make the code more readable
-
+        EXCLUDED_ROUTES = ["route_sign_in_or_up", "route_index", "route_favicon", "route_signing_css", "route_blob_image"]  # add routes as needed
+        route_to_load = request.endpoint  # To make the code more readable
+        print(f"route : {route_to_load}")
         if route_to_load not in EXCLUDED_ROUTES:
             if route_to_load is not None:
                 if session.get('login_state') is None or 'login_state' not in session:
                     return redirect("/sign.in.up")
                 elif session['login_state'] is True:
-                    pass # Continue and load request (any)
+                    pass  # Continue and load request (any)
                 else:
                     return redirect("/sign.in.up")
 
-            # Typically occurs when an error is triggered ex : 404.
+            # Typically occurs when an error is triggered ex : 404
             if route_to_load is None and session.get("login_state") != True:
                 return redirect("/sign.in.up")
-        else:
-            # If endpoint is signing page
-            if session.get('login_state') is not None and route_to_load == "route_sign_in_or_up":
-                return redirect("/home") # If user's connected he will be redirected to home
             # Else, continue and load request (from EXCLUDED_ROUTES)
+
+    @flask_app.before_request
+    def requests_counter():
+        globals()["request_counter"] += 1
+        print(f"request n°{globals()['request_counter']} since server was launched ")
+
+
+    @flask_app.route('/favicon.ico')
+    def route_favicon():
+        return send_from_directory(os.path.join(flask_app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+    @flask_app.route('/static/css/signing.css')
+    def route_signing_css():
+        return send_from_directory('static/css', 'signing.css')
+
+    @flask_app.route('/static/images/blob.svg')
+    def route_blob_image():
+        return send_from_directory('static/images', 'blob.svg')
+
 
     @flask_app.route("/", methods=["GET", "POST"])
     def route_index():
@@ -68,7 +87,7 @@ if __name__ == "__main__":
 
     @flask_app.errorhandler(404)
     def handle_404(error):
-        return render_template_string(f"{error} ")
+        return render_template("404.html")
 
 
-    flask_app.run(debug=True, host="0.0.0.0", port=5000)
+    flask_app.run(debug=True, host="0.0.0.0", port=5555)
